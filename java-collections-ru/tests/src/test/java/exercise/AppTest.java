d2aa077cfbca5deaa8c98afd6879a74914d6b849
@hexlet/cli <support@hexlet.io>
2023-01-25 01:08:40 +0500

    submit java-collections-ru/tests

---
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -0,0 +1,27 @@
+package exercise;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testTake() {
+        // BEGIN
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        List<Integer> result = App.take(list, 2);
+        Assertions.assertThat(result).hasSize(2)
+                .isEqualTo(Arrays.asList(list.get(0), list.get(1)));
+        // END
+    }
+}
